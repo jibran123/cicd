75599431e686d7c53aaab008611243266af9b49f
jibran123 <pateljibran2008@gmail.com>
2019-05-14 16:05:19 +0200

    Fixed minor bugs

--- scripts/updateManifestToUseNewlyCreatedDockerImage.sh
+++ scripts/updateManifestToUseNewlyCreatedDockerImage.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-#newVersion=`wget -q https://registry.hub.docker.com/v1/repositories/"$1"/"$2"/tags -O - | sed -e 's/[][]//g' -e 's/"//g' -e 's/ //g' | tr '}' '\n' | awk -F: '{print $3}' | tail -n 1`
-sed 's/newTag/1.1/g' k8-manifests/"$2"/"$2.yaml" > k8-manifests/"$2"/"$2_new.yaml"
+newVersion=`wget -q https://registry.hub.docker.com/v1/repositories/"$1"/"$2"/tags -O - | sed -e 's/[][]//g' -e 's/"//g' -e 's/ //g' | tr '}' '\n' | awk -F: '{print $3}' | tail -n 1`
+sed "s/newTag/$newVersion/g" k8-manifests/"$2"/"$2.yaml" > k8-manifests/"$2"/"$2_new.yaml"
 ls k8-manifests/"$2"/"$2_new.yaml"
 exitCode=`echo $?`
 if [[ $exitCode -eq 0 ]]; then
